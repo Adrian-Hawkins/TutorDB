11a03b9e1eeb3a255ab12ca1fb6e3ad3234e2b92
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-13 11:34:31 +0200

    Changed student deletion trigger

--- Triggers/DeleteStudent.sql
+++ Triggers/DeleteStudent.sql
@@ -1,11 +1,10 @@
 CREATE OR ALTER TRIGGER StudentDeletion
-ON [Students]
-AFTER DELETE
+ON [dbo].[Students]
+INSTEAD OF DELETE
 AS
 BEGIN
     SET NOCOUNT ON;
-    DELETE FROM [StudentSessions]
-    WHERE [student_id] IN (SELECT [id] FROM deleted);
-    DELETE FROM [Sessions]
-    WHERE [id] IN (SELECT [session_id] FROM deleted);
+    UPDATE [Students]
+    SET [deleted] = 1
+    WHERE [id] IN (SELECT [id] FROM deleted)
 END;
