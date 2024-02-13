c917111548399ccdbf082bb5d5786172eb81af01
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-13 10:10:29 +0200

    Removed defaults for timestamps

--- Tables/create_students.sql
+++ Tables/create_students.sql
@@ -2,6 +2,6 @@ CREATE TABLE [Students] (
   [id] integer PRIMARY KEY IDENTITY (1,1),
   [name] nvarchar(255) NOT NULL,
   [surname] nvarchar(255) NOT NULL,
-  [created_on] timestamp default (CURRENT_TIMESTAMP)
+  [created_on] timestamp
 );
 GO
