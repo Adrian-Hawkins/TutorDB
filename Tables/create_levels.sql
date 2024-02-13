5050807ce802b84ab69323cc60d7066839d7b4b4
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-13 10:10:29 +0200

    Removed defaults for timestamps

--- Tables/create_levels.sql
+++ Tables/create_levels.sql
@@ -2,6 +2,6 @@ CREATE TABLE [Level] (
   [id] integer PRIMARY KEY IDENTITY (1,1),
   [title] nvarchar(255) NOT NULL,
   [description] nvarchar(255) NOT NULL,
-  [created_on] timestamp default (CURRENT_TIMESTAMP)
+  [created_on] timestamp
 );
 GO
