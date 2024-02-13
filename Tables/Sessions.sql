1b556a600387ad87aa7c144f2d21b151660f0596
AdrianHawkins <adrianhawkins02@gmail.com>
2024-02-13 07:19:00 +0000

    Fixed Sessions table

--- Tables/Sessions.sql
+++ Tables/Sessions.sql
@@ -1,11 +1,12 @@
 CREATE TABLE [Sessions] (
-  [id] integer PRIMARY KEY,
+  [id] integer PRIMARY KEY IDENTITY (1,1),
   [tutor_subject_id] integer,
   [start] datetime NOT NULL,
   [duration_minutes] integer NOT NULL,
-  [price] double NOT NULL,
+  [price] decimal(18,2) NOT NULL,
   [link] nvarchar(255) NOT NULL,
   [capacity] integer,
-  [created_on] timestamp
+  [created_on] timestamp,
+  CONSTRAINT CHK_Price_Above_Zero CHECK (price > 0)
 )
 GO
