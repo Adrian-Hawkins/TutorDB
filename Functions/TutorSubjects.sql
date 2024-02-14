41eae1f2c057ea741972da6756fc1fea776381b8
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-02-14 10:07:01 +0200

    Updated TutorSubjects Function

--- Functions/TutorSubjects.sql
+++ Functions/TutorSubjects.sql
@@ -6,8 +6,10 @@ RETURNS TABLE
 AS
 RETURN
 (
-    SELECT s.id, s.name, s.level_id, s.material_link
-    FROM TutorSubjects ts
-    INNER JOIN Subjects s ON ts.subject_id = s.id
+    SELECT t.name, t.surname, s.name, l.title, s.material_link
+	FROM Level l
+    INNER JOIN Subjects s ON s.level_id = l.id
+    INNER JOIN TutorSubjects ts ON ts.subject_id = s.id
+	INNER JOIN Tutors t ON ts.tutorId = t.id
     WHERE ts.tutor_id = @tutorId
 );
