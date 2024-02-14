de01d1783807a52a54a2381ae13e944f82644093
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-14 16:31:48 +0200

    test field

--- flyway/TutorDB/migrations/create_levels.sql
+++ flyway/TutorDB/migrations/create_levels.sql
@@ -2,6 +2,7 @@ CREATE TABLE [Levels] (
   [id] integer PRIMARY KEY IDENTITY (1,1),
   [title] nvarchar(255) NOT NULL,
   [description] nvarchar(255) NOT NULL,
+  [testfield] nvarchar(255),
   [created_on] timestamp
 );
 GO
