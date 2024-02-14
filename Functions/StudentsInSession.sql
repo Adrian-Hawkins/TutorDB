b2ffebe53673740b04b13bd4b324f3b5fe58dd94
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-02-14 15:23:02 +0200

    Added table value functions

---
+++ Functions/StudentsInSession.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION dbo.GetStudentsInSession
+(
+    @sessionID INT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT s.name, s.surname FROM Students s INNER JOIN StudentSessions ss ON s.id = ss.student_id WHERE ss.id = @sessionID
+);
