00e83e8a5ab624eea4246ae3d2867cb84296be59
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-14 16:34:15 +0200

    removed test migration

--- flyway/TutorDB/migrations/create_levels.sql
+++ flyway/TutorDB/migrations/create_levels.sql
@@ -2,7 +2,6 @@ CREATE TABLE [Levels] (
   [id] integer PRIMARY KEY IDENTITY (1,1),
   [title] nvarchar(255) NOT NULL,
   [description] nvarchar(255) NOT NULL,
-  [testfield] nvarchar(255),
   [created_on] timestamp
 );
 GO
