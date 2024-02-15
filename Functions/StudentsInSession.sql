c01c7f084dad50be4053e4d5ba2d76a0164b200a
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-02-15 15:34:54 +0200

    Fixed getStudentsInSession function

--- Functions/StudentsInSession.sql
+++ Functions/StudentsInSession.sql
@@ -6,5 +6,5 @@ RETURNS TABLE
 AS
 RETURN
 (
-    SELECT s.name, s.surname FROM Students s INNER JOIN StudentSessions ss ON s.id = ss.student_id WHERE ss.id = @sessionID
+    SELECT s.name, s.surname FROM Students s INNER JOIN StudentSessions ss ON s.id = ss.student_id WHERE ss.session_id = @sessionID
 );
