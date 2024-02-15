5f97ef84b677dbaaff6bf4d67cfce6c0ee6454a9
Adrian Hawkins <Adrian.Hawkins@bbd.co.za>
2024-02-15 11:54:16 +0200

    Updated varchars

--- Tables/create_subjects.sql
+++ Tables/create_subjects.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [Subjects] (
     [id] integer PRIMARY KEY IDENTITY (1,1),
-    [name] nvarchar(255) NOT NULL,
+    [name] varchar(50) NOT NULL,
     [level_id] integer NOT NULL,
-    [material_link] nvarchar(255) NOT NULL,
+    [material_link] varchar(255) NOT NULL,
     [created_on] timestamp
 );
 GO
